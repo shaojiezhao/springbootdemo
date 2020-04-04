8e9c47974e6f3593c98b4c75b9534b4d2c6043d9
jack zhao <466711305@qq.com>
2020-04-04 13:51:29 +0800

    add new repository

---
+++ src/main/java/com/example/springbootdemo/demo/ApiController.java
@@ -0,0 +1,19 @@
+package com.example.springbootdemo.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController
+{
+    @Value("${girl.name}")
+    private String girlName;
+
+    @GetMapping("/hello")
+    public String sayHello()
+    {
+        return "hello " + girlName;
+    }
+
+}
