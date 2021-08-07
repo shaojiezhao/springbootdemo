f5291a573424ceaf05efb14f9ce55ab5822df9a0
jack zhao <466711305@qq.com>
2021-08-07 16:00:45 +0800

    commit send email codes.

---
+++ send-email-report/src/main/java/com/hsbc/trade/sendemailreport/SendEmailReportApplication.java
@@ -0,0 +1,15 @@
+package com.hsbc.trade.sendemailreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendEmailReportApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SendEmailReportApplication.class, args);
+    }
+
+}
---
+++ send-email-report/src/test/java/com/hsbc/trade/sendemailreport/SendEmailReportApplicationTests.java
@@ -0,0 +1,15 @@
+package com.hsbc.trade.sendemailreport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendEmailReportApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
